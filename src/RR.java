334546a80fff2e6b710a6f8472d0e1502efc7b01
johnszyna <jan.szyndlarewicz@gmail.com>
2023-03-19 00:07:46 +0100

    RoundRobin
    Took 34 minutes

--- src/RR.java
+++ src/RR.java
@@ -3,7 +3,38 @@ import java.util.ArrayList;
 public class RR implements Algorithm{
     public RR(){};
 
-    public void doAlgorithm(ArrayList<Process> processArrayList){
+    public void doAlgorithm(ArrayList<Process> queueList){
+        int realTime = 0;
+        int QUANTUM_TIME = 5;
+        int counterOfFinishedProcesses = 0;
+        int iterator = 0;
 
+        while (counterOfFinishedProcesses < queueList.size()){
+
+            Process currentProcess = queueList.get(iterator);
+            if(currentProcess.getFILLING_TIME() <= realTime && currentProcess.getLengthLeft() != 0){
+                //System.out.println("Test -> " + currentProcess);
+                if(currentProcess.getLengthLeft()-QUANTUM_TIME <= 0){
+                    realTime += currentProcess.getLengthLeft();
+                    currentProcess.setLengthLeft(0);
+                    currentProcess.setFinishTime(realTime-currentProcess.getFILLING_TIME());
+                    counterOfFinishedProcesses++;
+                }
+                else {
+                    currentProcess.setLengthLeft(currentProcess.getLengthLeft()-QUANTUM_TIME);
+                    realTime += QUANTUM_TIME;
+                }
+
+            }
+            else {
+                realTime++;
+            }
+
+            iterator++;
+
+            if(iterator >= queueList.size()) iterator = 0;
+
+
+        }
     }
 }
